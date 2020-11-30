9bc41c49ae7088ae4dac4ae73add5bf0890523f6
MahimaJojee <mahima.jojee@gmail.com>
2020-11-30 16:33:32 +0530

    init setup

---
+++ CabMS/src/test/java/com/cts/proj/CabMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
